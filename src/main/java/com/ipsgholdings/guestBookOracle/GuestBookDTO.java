d57455b07fff68831c21ac6097d138a659bb7809
Jun Tachikawa <2023.04.j.tachikawa@gmail.com>
2023-07-27 10:45:16 +0900

    ログイン用のDTO作成

--- src/main/java/com/ipsgholdings/guestBookOracle/GuestBookDTO.java
+++
@@ -1,5 +0,0 @@
-package com.ipsgholdings.guestBookOracle;
-
-public class GuestBookDTO {
-
-}
---
+++ src/main/java/com/ipsgholdings/guestBookOracle/MemberDTO.java
@@ -0,0 +1,49 @@
+package com.ipsgholdings.guestBookOracle;
+
+public class MemberDTO {
+	//ユーザー番号
+	private int UserID;
+	//パスワード
+	private String password;
+	//名前
+	private String UserName;
+	
+	
+	//コンストラクタ
+	/**
+	 * @param userID
+	 * @param password
+	 * @param userName
+	 */
+	public MemberDTO(int userID, String password, String userName) {
+		super();
+		UserID = userID;
+		this.password = password;
+		UserName = userName;
+	}
+	
+	
+	//	getter/setter
+	public int getUserID() {
+		return UserID;
+	}
+	public void setUserID(int userID) {
+		UserID = userID;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getUserName() {
+		return UserName;
+	}
+	public void setUserName(String userName) {
+		UserName = userName;
+	}
+	
+	
+	
+	
+}
