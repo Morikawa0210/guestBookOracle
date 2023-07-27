b8b48cf3cb983829b3fd0db1f37bcc9e22e3a2fb
Kazuhito Morikawa <2023.04.k.morikawa@gmail.com>
2023-07-27 10:06:48 +0900

    初めてのGIT

---
+++ src/main/java/com/ipsgholdings/guestBookOracle/GuestBookOracleApplication.java
@@ -0,0 +1,13 @@
+package com.ipsgholdings.guestBookOracle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuestBookOracleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuestBookOracleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ipsgholdings/guestBookOracle/GuestBookOracleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipsgholdings.guestBookOracle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuestBookOracleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
